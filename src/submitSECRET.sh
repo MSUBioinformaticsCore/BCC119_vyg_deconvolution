2fa9fc443ef2c56c384ef4f3c91fa37126bfffb2
slepphickey <slepphickey@gmail.com>
2024-10-29 09:48:29 -0400

    update

--- src/submitSECRET.sh
+++ src/submitSECRET.sh
@@ -2,7 +2,7 @@
 #SBATCH --mem=100GB
 #SBATCH --job-name=SECRET
 #SBATCH --output=%x-%j.out
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --account=bioinformaticscore
 #SBATCH --constrain=intel18
