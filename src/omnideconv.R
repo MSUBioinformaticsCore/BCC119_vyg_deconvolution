140446b879fed3913c50abfd039cd433ad9eeaf7
slepphickey <slepphickey@gmail.com>
2024-10-30 16:29:16 -0400

    update

--- src/omnideconv.R
+++ src/omnideconv.R
@@ -109,7 +109,7 @@ if(tool == "scdc"){
                                            cell_type_annotations = cell.type.annotations, 
                                            batch_ids = batch.ids,
                                            quality_control = TRUE,
-                                           ct.sub = colnames(seuratOb))
+                                           ct_sub = colnames(seuratOb))
   
   saveRDS(deconvolution, file = paste0(tool, "_deconvolution_withQC.rds"))
   
@@ -118,8 +118,7 @@ if(tool == "scdc"){
                                            single_cell_object = single_cell_object, 
                                            cell_type_annotations = cell.type.annotations, 
                                            batch_ids = batch.ids,
-                                           quality_control = FALSE,
-                                           ct.sub = colnames(seuratOb))
+                                           quality_control = FALSE)
   
   saveRDS(deconvolution, file = paste0(tool, "_deconvolution_noQC.rds"))
   
