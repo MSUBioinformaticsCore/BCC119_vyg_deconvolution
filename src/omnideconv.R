31f182fe43f0abaad820c38bccc488d6beb4e59b
slepphickey <slepphickey@gmail.com>
2024-10-30 10:45:13 -0400

    add

---
+++ src/omnideconv.R
@@ -0,0 +1,131 @@
+#https://github.com/omnideconv/omnideconv/blob/main/vignettes/omnideconv_example.Rmd
+
+library(tidyverse)
+library(Seurat)
+library(omnideconv)
+
+args <- commandArgs(TRUE)
+
+ref.seurat.path = args[1] 
+bulk.mtx.path = args[2]
+tool = args[3]
+annotation.col = args[4]
+batch.col = args[5]
+results.dir = args[6]
+
+setwd(results.dir)
+print(tool)
+
+# Set up ------------------------------------------------------------------
+
+# load bulk data
+bulk.mtx = readRDS(bulk.mtx.path)
+
+# load reference data
+seuratOb = readRDS(ref.seurat.path)
+
+
+# Subsample reference -----------------------------------------------------
+
+max_cells_per_celltype = 200
+
+sampled.metadata <- seuratOb@meta.data %>%
+  rownames_to_column(., 'barcode') %>%
+  group_by(., annotation.col) %>% 
+  nest() %>%            
+  mutate(n =  map_dbl(data, nrow)) %>%
+  mutate(n = min(n, max_cells_per_celltype)) %>%
+  ungroup() %>% 
+  mutate(samp = map2(data, n, sample_n)) %>% 
+  select(-data) %>%
+  unnest(samp)
+
+single.cell.data.sampled <- subset(seuratOb, cells = sampled.metadata$barcode)
+
+# DWLS --------------------------------------------------------------------
+
+if(tool == "dwls"){
+  
+  single_cell_object <- as.matrix(single.cell.data.sampled[["RNA"]]$counts)
+  cell.type.annotations <- sampled.metadata %>% pull(all_of(annotation.col))
+  batch.ids <- sampled.metadata %>% pull(all_of(batch.col))
+  
+  signature <- omnideconv::build_model(single_cell_object,
+                                       cell_type_annotations = cell.type.annotations,
+                                       batch_ids = batch.ids, 
+                                       method = "dwls", 
+                                       dwls_method = 'mast_optimized')
+  
+  saveRDS(signature, file = paste0(tool, "_signature.rds"))
+  
+  deconvolution <- omnideconv::deconvolute(bulk_gene_expression = bulk.mtx, 
+                                           signature,
+                                           method='dwls',
+                                           dwls_submethod = 'DampenedWLS')
+  
+  saveRDS(deconvolution, file = paste0(tool, "_deconvolution.rds"))
+  
+}
+
+
+# scaden ------------------------------------------------------------------
+
+if(tool == "scaden"){
+  
+  single_cell_object <- as.matrix(single.cell.data.sampled[["RNA"]]$counts)
+  cell.type.annotations <- sampled.metadata %>% pull(all_of(annotation.col))
+  batch.ids <- sampled.metadata %>% pull(all_of(batch.col))
+  
+  signature <- omnideconv::build_model(single_cell_object,
+                                       cell_type_annotations = cell.type.annotations,
+                                       batch_ids = batch.ids, 
+                                       method = "scaden", 
+                                       bulk_gene_expression = bulk.mtx)
+  
+  saveRDS(signature, file = paste0(tool, "_signature.rds"))
+  
+  deconvolution <- omnideconv::deconvolute(bulk_gene_expression = bulk.mtx, 
+                                           signature,
+                                           method = "scaden")
+  
+  saveRDS(deconvolution, file = paste0(tool, "_deconvolution.rds"))
+  
+}
+
+
+# SCDC --------------------------------------------------------------------
+
+if(tool == "scdc"){
+  
+  single_cell_object <- as.matrix(seuratOb[["RNA"]]$counts)
+  cell.type.annotations <- seuratOb@metadata %>% pull(all_of(annotation.col))
+  batch.ids <- seuratOb@metadata %>% pull(all_of(batch.col))
+
+  deconvolution <- omnideconv::deconvolute(bulk_gene_expression = bulk.mtx,
+                                           method = "scdc",
+                                           single_cell_object, 
+                                           cell_type_annotations, 
+                                           batch_ids)
+  
+  saveRDS(deconvolution, file = paste0(tool, "_deconvolution.rds"))
+  
+}
+
+# music --------------------------------------------------------------------
+
+if(tool == "music"){
+  
+  single_cell_object <- as.matrix(seuratOb[["RNA"]]$counts)
+  cell.type.annotations <- seuratOb@metadata %>% pull(all_of(annotation.col))
+  batch.ids <- seuratOb@metadata %>% pull(all_of(batch.col))
+  
+  deconvolution <- omnideconv::deconvolute(bulk_gene_expression = bulk.mtx,
+                                           method = "music",
+                                           single_cell_object, 
+                                           cell_type_annotations, 
+                                           batch_ids)
+  
+  saveRDS(deconvolution, file = paste0(tool, "_deconvolution.rds"))
+  
+}
+
---
+++ src/submit_omnideconv.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -login
+#SBATCH --mem=100GB
+#SBATCH --job-name=omnideconv
+#SBATCH --output=%x-%j.out
+#SBATCH --time=4:00:00
+#SBATCH --nodes=1
+#SBATCH --account=bioinformaticscore
+#SBATCH --constrain=intel18
+
+################################################################## 
+# Print parameters
+##################################################################
+
+start=`date +%s`
+echo "omnideconv.R path:" $1
+echo "ref.seurat.path:" $2
+echo "bulk.mtx.path:" $3
+echo "tool:" $4
+echo "annotation.col:" $5
+echo "batch.col:" $6
+echo "results.dir:" $7
+
+################################################################## 
+# Run omnideconv
+##################################################################
+
+module purge
+module load R-bundle-CRAN/2023.12-foss-2023a
+
+Rscript $1 $2 $3 $4 $5 $6 $7
+
+################################################################## 
+# Finish
+##################################################################
+
+echo "Finished"
+end=`date +%s`
+runtime=$((end-start))
+echo execution time was `expr $end - $start` 
