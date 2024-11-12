4789fd7ab42a6fd184974d71f55c4a2ae61e422c
slepphickey <slepphickey@gmail.com>
2024-11-12 16:04:13 -0500

    add to remove specified cell types from reference

---
+++ src/filterCellTypes.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(Seurat)
+
+# Capture command line arguments
+args <- commandArgs(TRUE)
+
+# Define paths and parameters from arguments
+ref.seurat.path = args[1]             # Path to Seurat reference data
+annotation.col = args[2]              # Column with cell type annotations
+remove.types = strsplit(args[3], ",") # cell types to discard 
+
+# Set working directory to results directory
+outdir = dirname(ref.seurat.path)
+setwd(out.dir)
+
+# Load Seurat reference object (single-cell data)
+seuratOb = readRDS(ref.seurat.path)
+
+# filter seurat object for cell types of interest
+cells_oi =
+  seuratOb@meta.data %>%
+  filter(!(!!sym(annotation.col) %in% remove.types)) 
+
+seuratOb_oi <- subset(seuratOb, cells = rownames(cells_oi))
+
+new_file = gsub("^(.*)[.].*", "\\1", ref.seurat.path)
+new_file = paste0(new_file, "_cells_oi.rds")
+saveRDS(seuratOb_oi, file = new_file)
